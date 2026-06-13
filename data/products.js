373fe8206462f5e6ed15eb536d93d0b41644d257
manalooo <manal.issa.pro@gmail.com>
2026-06-13 16:05:53 +0300

    Fix: mark only Coral Roma Blazer (wear-35) as sold; revert wear-33

--- data/products.js
+++ data/products.js
@@ -626,7 +626,7 @@ window.PRODUCTS = [
       "images/jookh/wearables/piece-33-view-1.jpg",
       "images/jookh/wearables/piece-33-view-2.jpg"
     ],
-    "status": "sold",
+    "status": "available",
     "subcollection": "Roma"
   },
   {
@@ -656,7 +656,7 @@ window.PRODUCTS = [
       "images/jookh/wearables/piece-35-view-1.jpg",
       "images/jookh/wearables/piece-35-view-2.jpg"
     ],
-    "status": "available",
+    "status": "sold",
     "subcollection": "Roma"
   },
   {
